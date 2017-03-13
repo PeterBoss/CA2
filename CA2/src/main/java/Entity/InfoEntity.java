47d37dddd7281ff5e0c9ce8f743307c0557ab140
PeterBoss <peter2137@gmail.com>
2017-03-13 14:56:38 +0100

    InfoEntity class & project start

---
+++ CA2/src/main/java/Entity/InfoEntity.java
@@ -0,0 +1,42 @@
+package Entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Peter
+ */
+@Entity
+public class InfoEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String email;
+
+    public InfoEntity() {
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
